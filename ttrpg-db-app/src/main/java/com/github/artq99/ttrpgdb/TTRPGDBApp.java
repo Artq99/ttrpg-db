60d0ac3222ccf0fcea7c013422ec9c99a90f3cb7
Artq99 <amatracki@gmail.com>
2023-11-18 00:07:20 +0100

    Initial commit

---
+++ ttrpg-db-app/src/main/java/com/github/artq99/ttrpgdb/TTRPGDBApp.java
@@ -0,0 +1,12 @@
+package com.github.artq99.ttrpgdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TTRPGDBApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TTRPGDBApp.class);
+    }
+}
